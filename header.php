db26b1adb6dc516edf5b2e737b891021219f24a2
Thomas11x <marczakowsky54@gmail.com>
2022-12-03 18:10:43 +0100

    dodanie sesji oraz zmiennych globalnych

--- header.php
+++ header.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+?>
+
+
 <!-- === NAVBAR === -->
 
     <!-- TOP NAVBAR -->
--- zaloguj.php
+++ zaloguj.php
@@ -1,15 +1,40 @@
 <?php
 
+session_start();
+
 require_once "connect.php";
 
 $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
 
 if($polaczenie->connect_errno!=0){
-    echo "ERROR: " . $polaczenie->connect_errno . "Opis: " . $polaczenie->connect_errno; 
+    echo "ERROR: " . $polaczenie->connect_errno; 
 }
 else{
     $login = $_POST['login'];
-    $haslo = $_POST['password'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM user WHERE login='$login' AND password = '$password'";
+
+    if($rezultat = @$polaczenie->query($sql)){
+        $many_users = $rezultat->num_rows;
+        if($many_users>0){
+            $wiersz = $rezultat->fetch_assoc();
+            $_SESSION['user'] = $wiersz['login'];
+            $_SESSION['e-mail'] = $wiersz['e-mail'];
+            $_SESSION['imie'] = $wiersz['imie']; 
+            $_SESSION['nazwisko'] = $wiersz['nazwisko']; 
+            $_SESSION['miasto'] = $wiersz['miasto']; 
+            $_SESSION['kod_pocztowy'] = $wiersz['kod_pocztowy'];
+            $_SESSION['ulica'] = $wiersz['ulica'];
+            $_SESSION['nr_domu'] = $wiersz['nr_domu'];
+            
+            $rezultat->free_result();
+            header('Location: index.php');
+        }
+        else {
+
+        }
+    }
 
     $polaczenie->close();
 }
