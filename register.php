c069f61e069d0a1d62c637d6314b476b55aa715e
Thomas11x <marczakowsky54@gmail.com>
2022-12-06 23:23:57 +0100

    register email

--- register.php
+++ register.php
@@ -29,6 +29,48 @@ if (isset($_POST['email'])){
         $_SESSION['e_email'] = "Podaj poprawny adres e-mail";
     }
 
+    //password check 
+    $haslo1 = $_POST['password'];
+    $haslo2 = $_POST['password2'];
+    if((strlen($haslo1)<8) ||(strlen($haslo1)>20)){
+        $correct = false;
+        $_SESSION['e_haslo'] = 'Hasło musi posiadać od 8 do 20 znaków';
+    }
+
+    if ($haslo1 !=$haslo2){
+        $correct = false;
+        $_SESSION['e_haslo'] = 'Podane hasła nie są identyczne';
+    }
+
+    $haslo_hash = password_hash($haslo1, PASSWORD_DEFAULT);
+    
+
+    require_once "connect.php";
+    mysqli_report(MYSQLI_REPORT_STRICT);
+
+    try {
+        $polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+        if($polaczenie->connect_errno!=0){
+            throw new Exception(mysqli_connect_errno());
+        }
+        else
+        {   
+            //Czy email już istnieje?
+            $rezultat = $polaczenie->query("SELECT id FROM user WHERE email='$email'");
+
+            if (!$rezultat) throw new Exception($polaczenie->error);
+            $ile_takich_maili = $rezultat->num_rows;
+            if($ile_takich_maili>0){
+                $correct = false;
+                $_SESSION['e_email'] = 'Istnieje już konto przypisane do tego adresu e-mail!';
+            }
+
+            $polaczenie->close();
+        }
+    }
+    catch(Exception $e) {
+        echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o rejestracje w innym terminie </span>';
+    }
 
     if ($correct == true){
         echo "Udana walidacja!";
@@ -101,6 +143,11 @@ if (isset($_POST['email'])){
                     <div class="mb-3">
                         <label for="password">Hasło *</label>
                         <input class="form-control" type="password" name="password" id="password" required="required" data-error="Hasło jest wymagane">
+                        <?php if (isset($_SESSION['e_haslo'])){
+                            echo '<div class ="error">' . $_SESSION['e_haslo'] . '</div>'; // POPRAWKI STYLU
+                            unset($_SESSION['e_haslo']);
+                        }
+                        ?>
                         <div class="help-block with-errors"></div>
                     </div>
                     <div class="mb-3">
