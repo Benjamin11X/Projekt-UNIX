f6a310dc22d04393267094ba898aeb24accc6630
Benjamin11X <konrad.michalak2003work@gmail.com>
2022-12-06 19:30:20 +0100

    XD

---
+++ account.php
@@ -0,0 +1,40 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" defer></script>
+    
+    <!-- FONT AWESOME -->
+    <script src="https://kit.fontawesome.com/c2d11256b6.js" crossorigin="anonymous" defer></script>
+
+    <!-- IMPORTING STYLES -->
+    <link rel="stylesheet" href="assets/styles/style.css">
+    
+    <title>UNIX</title>
+</head>
+<body>
+    <!-- HEADER -->
+    <?php include 'header.php'; ?>
+
+    <!-- CONTENT -->
+    <div class="container-fluid">
+        <div class="container-lg d-flex">
+            <div class="account">
+                .account
+            </div>
+        </div>
+    </div>
+
+    <!-- FOOTER -->
+    <?php include 'footer.php'; ?>
+</body>
+</html>
